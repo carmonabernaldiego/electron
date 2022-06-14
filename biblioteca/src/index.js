02744815ae43fe67f4a911c32ebc5f93c51742ee
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-06-14 11:46:30 -0500

    update

--- biblioteca/src/index.js
+++ biblioteca/src/index.js
@@ -20,7 +20,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'login.html'));
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
