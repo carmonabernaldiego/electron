f9b426f8d20de0f05dcd12a6ab64bd3a822c76f6
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-06-28 10:50:09 -0500

    update

--- biblioteca/src/views/load-user.js
+++ biblioteca/src/views/load-user.js
@@ -24,6 +24,7 @@ class Page {
         let profileUserBox = this.get('#profileUserBox');
         let profileName = this.get('#profileName');
         let profileEmail = this.get('#profileEmail');
+        let navUser = this.get('#navUser');
 
         window.ipcRender.invoke('getUserData').then((result) => {
             const { user, email, permissions, image, name, surnames } = result;
@@ -34,7 +35,36 @@ class Page {
                 profileName.innerHTML = name + ' ' + surnames;
                 profileEmail.innerHTML = email;
 
-                
+                let texto =
+                    `
+                    <li class="nav-item nav-category">Estantes</li>
+                    <li class="nav-item">
+                      <a href="consultar.html" class="nav-link">
+                        <i class="link-icon" data-feather="search"></i>
+                        <span class="link-title">Consultar Libros</span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="agregar.html" class="nav-link">
+                        <i class="link-icon" data-feather="plus-circle"></i>
+                        <span class="link-title">Agregar Libros</span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="modificar.html" class="nav-link">
+                        <i class="link-icon" data-feather="edit"></i>
+                        <span class="link-title">Modificar Libros</span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="eliminar.html" class="nav-link">
+                        <i class="link-icon" data-feather="delete"></i>
+                        <span class="link-title">ELiminar Libros</span>
+                      </a>
+                    </li>
+                `;
+
+                navUser.innerHTML += texto;
             } else if (permissions == 'invitado') {
                 profileName.innerHTML = 'Invitado';
                 profileEmail.innerHTML = 'invitado@ckh.com';
