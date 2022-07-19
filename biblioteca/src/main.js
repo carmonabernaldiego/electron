62a3babcb36b2e9d96588376cea9bef992833873
Diego Carmona Bernal <cbdiegox@gmail.com>
2022-07-18 20:54:44 -0500

    u

--- biblioteca/src/main.js
+++ biblioteca/src/main.js
@@ -26,7 +26,7 @@ const createWindowDashboard = () => {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
-      devTools: true,
+      devTools: false,
       preload: path.join(__dirname, 'preload.js')
     }
   });
